587df1525c6b6ab69666140c705ad167f18f058b
John Chernoff <jachernoff@gmail.com>
2024-10-27 22:35:16 -0700

    added cancel box to pieceDialogs

--- lib/dialogs.dart
+++ lib/dialogs.dart
@@ -2,23 +2,30 @@ import 'package:flutter/material.dart';
 import 'package:zug_chess/zug_chess.dart';
 
 class ChessDialogs {
-  static Future<Piece?> pieceDialog(List<Piece> pieces, BuildContext context, {
-    msg = 'Choose a piece'}) async {
+  static Future<Piece?> pieceDialog(List<Piece> pieces, BuildContext context, {double? width = 800, msg = 'Choose a piece', Image? cancelImg}) async {
     return showDialog<Piece>(
       context: context,
       barrierDismissible: false,
       builder: (BuildContext context) {
+        double? choiceWidth;
+        if (width != null) choiceWidth = width / (pieces.length + 1);
+        List<Widget> choices = [InkWell(
+          child: SizedBox(width: choiceWidth, child: cancelImg ?? const SizedBox.shrink()),
+          onTap: () {
+            Navigator.of(context).pop(null);
+          },
+        ), ...List.generate(pieces.length, (i) =>  InkWell(
+          child: SizedBox(width: choiceWidth, child: Piece.imgMap[pieces.elementAt(i).toString()]),
+          onTap: () {
+            Navigator.of(context).pop(pieces.elementAt(i));
+          },
+        ))];
         return AlertDialog(
           title: Text(msg),
-          content: SizedBox(width: 640, child: SingleChildScrollView(scrollDirection: Axis.horizontal,
+          content: SizedBox(width: width, child: SingleChildScrollView(scrollDirection: Axis.horizontal,
               child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: List.generate(pieces.length, (i) =>  InkWell(
-                child: Piece.imgMap[pieces.elementAt(i).toString()],
-                onTap: () {
-                  Navigator.of(context).pop(pieces.elementAt(i));
-                },
-              ))
+              children: choices
           ))),
         );
       },
